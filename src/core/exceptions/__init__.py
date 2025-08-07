ff367291bc3a4b5399b3663611320a7cc536c2ff
victormacedo996 <victormacedo996@gmail.com>
2025-08-06 22:40:47 -0300

    feat: add PlatformNotSupportedException

---
+++ src/core/exceptions/__init__.py
@@ -0,0 +1,3 @@
+from core.exceptions.platform_not_supported import PlatformNotSupportedException
+
+__all__ = ["PlatformNotSupportedException"]
---
+++ src/core/exceptions/platform_not_supported.py
@@ -0,0 +1,2 @@
+class PlatformNotSupportedException(Exception):
+    pass
